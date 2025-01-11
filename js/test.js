efe4668e1fd73ab5f39e15bbd9db1f178febc0c6
st:prchal <steveruu@stvr.cz>
2025-01-11 20:35:52 +0100

    File upload from old commit

---
+++ js/test.js
@@ -0,0 +1,22 @@
+function tvojeMama() {
+    var x = window.prompt("WgaT???????")
+    alert(x);
+    var smrdis;
+    var lil; {
+        var ash = 8;
+        var peep = null;
+        var pot = 69;
+    }
+}
+
+function onPageLoad() {
+    tvojeMama.smrdis = 13;
+    
+    lil.ash = 8;
+    console.log(lil.peep);
+    console.log(lil.pot);
+}
+
+
+
+
